38f8e98b521cd8fce95cb9d60841f9e196e6cdfd
Dmitry Galinsky <dima.exe@gmail.com>
2008-06-30 06:11:04 +0400

    Clean.

--- tests/core-ext.test.el
+++ tests/core-ext.test.el
@@ -4,18 +4,21 @@
 (defsuite core-ext ext)
 
 (deftest when-bind-with-run-block core-ext
+  "Test `when-bind' with block passed."
   (when-bind (val t)
     (assert-equal t val))
   (when-bind (val (emacs-version))
     (assert-equal (emacs-version) val)))
 
 (deftest when-bind-with-skip-block core-ext
+  "Test `when-bind' with block skiped block."
   (let ((res t))
     (when-bind (val nil)
       (setq res nil))
       (assert-equal t res)))
 
 (deftest define-keys-macro core-ext
+  "Test define keys using `define-keys' macro."
   (let ((map (make-sparse-keymap)))
     (assert
      (define-keys map
