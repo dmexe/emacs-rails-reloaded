52be0772b8cb58d8f5e34aaef9296ad571f53b29
dima <dima.exe@gmail.com>
2008-07-14 13:04:10 +0400

    UPdated webserver bundle and proxy.

--- tests/ext/test-helper.el
+++
@@ -1,7 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; core-ext
-(defun define-keys-test-map ()
-  (let ((map (make-sparse-keymap)))
-    (define-keys map
-      ("\C-c a" 'foo)
-      ("\C-c b" 'bar))))
